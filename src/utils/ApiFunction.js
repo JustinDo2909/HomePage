e09afbceb0450e3eb7bc5e1cd7a8c3b13289af34
JustinDo2909 <hieudeptraivodichh@gmail.com>
2024-07-23 21:44:13 +0700

    ok

--- src/utils/ApiFunction.js
+++ src/utils/ApiFunction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8081/DiamondShop",
+  baseURL: "https://diamond-shop-0fba6488d8a2.herokuapp.com/DiamondShop",
   withCredentials: true,
 });
 
